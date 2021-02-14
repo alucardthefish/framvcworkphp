970a1336917825f73126b145b477a9018be07f92
alucardthefish <sergiop18@gmail.com>
2021-02-14 18:37:57 -0500

    Add contact form

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,18 @@
 <h1>Contact Us</h1>
+
+
+<form>
+  <div class="mb-3">
+    <label for="subjectInput" class="form-label">Subject</label>
+    <input type="text" class="form-control" name="subject" id="subjectInput">
+  </div>
+  <div class="mb-3">
+    <label for="emailInput" class="form-label">Email</label>
+    <input type="text" class="form-control" id="emailInput">
+  </div>
+  <div class="mb-3">
+    <label for="bodyInput" class="form-label">Body</label><br>
+    <textarea cols="100" rows="10" name="body" id="bodyInput"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
