a7e207f5d458ea46fef31bf8a7f7913cbcdd5ad7
alucardthefish <sergiop18@gmail.com>
2021-02-23 21:30:38 -0500

    Add Database public attribute and update Application constructor method

--- core/Application.php
+++ core/Application.php
@@ -17,16 +17,20 @@ class Application {
 	public $router;
 	public $request;
 	public $response;
+	public $db;
+
 	public static $app;
 	public $controller;
 	
-	public function __construct($root_path) {
+	public function __construct($root_path, $config) {
 
 		self::$ROOT_DIR = $root_path;
 		self::$app = $this;
 		$this->request = new Request();
 		$this->response = new Response();
 		$this->router = new Router($this->request, $this->response);
+
+		$this->db = new Database($config['db']);
 
 	}
 
