d077174cbf56c1190928718f98c85fe7c82c6e0a
alucardthefish <sergiop18@gmail.com>
2021-02-23 21:28:19 -0500

    Add Database class for managing db connections

---
+++ core/Database.php
@@ -0,0 +1,29 @@
+<?php
+# **************************************************************************** 
+# File: Database.php 
+# Author: alucardthefish 
+# Created: Tue Feb 23 19:19:17 2021 
+# Description: Class that will be in charge of handle database connection 
+# **************************************************************************** 
+
+namespace app\core;
+
+use PDO;
+
+class Database
+{
+
+    public $pdo;
+
+    public function __construct($config) {
+        $dsn = $config['dsn'] ??  '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+        $this->pdo = new PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+}
+
+
+?>
+
