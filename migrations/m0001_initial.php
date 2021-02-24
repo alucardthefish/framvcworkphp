1f81361f8c34aa7a186b45231bbdf349de657fdd
alucardthefish <sergiop18@gmail.com>
2021-02-23 21:32:45 -0500

    Add migrations initial class and Install env variables loader

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,24 @@
+<?php
+# **************************************************************************** 
+# File: m0001_initial.php 
+# Author: alucardthefish 
+# Created: Tue Feb 23 19:14:09 2021 
+# Description: Migration initial file 
+# **************************************************************************** 
+
+class m0001_initial
+{
+    public function up()
+    {
+        echo "Applying migration";
+    }
+
+    public function down()
+    {
+        echo "Down migration";
+    }
+}
+
+
+?>
+
