870275a4ad3d34e1fd4f14917f4d1b1ea23b2421
alucardthefish <sergiop18@gmail.com>
2021-03-06 00:40:16 -0500

    Implement migrations

---
+++ migrations.php
@@ -0,0 +1,34 @@
+<?php
+# **************************************************************************** 
+# File: index.php 
+# Author: alucardthefish 
+# Created: Sat Feb  6 19:35:53 2021 
+# Description: Initial point to access the framework
+# **************************************************************************** 
+
+use app\controllers\SiteController;
+use app\controllers\AuthController;
+use app\core\Application;
+use Dotenv\Dotenv;
+
+require_once __DIR__.'/vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+$app = new Application(__DIR__, $config);
+
+
+$app->db->applyMigrations();
+
+
+?>
+
