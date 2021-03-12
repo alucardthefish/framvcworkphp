ca6a62bfa8e938dfb3a9a85df41588b7c1cf3519
alucardthefish <sergiop18@gmail.com>
2021-03-11 20:44:32 -0500

    Update User class to extend from UserModel instead of DbModel

--- models/User.php
+++ models/User.php
@@ -8,9 +8,9 @@
 
 namespace app\models;
 
-use app\core\DbModel;
+use app\core\UserModel;
 
-class User extends DbModel {
+class User extends UserModel {
 
     const STATUS_INACTIVE = 0;
     const STATUS_ACTIVE = 1;
@@ -65,6 +65,11 @@ class User extends DbModel {
     public static function primaryKey()
     {
         return 'id';
+    }
+
+    public function getDisplayName()
+    {
+        return $this->firstname . ' ' . $this->lastname;
     }
 }
 
